6b729a7ef4244f1c02682cb21c8fbcb43edf1463
Karmello <nogakamil@vp.pl>
2017-07-10 16:50:45 +0200

    updated

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # mean-start-off
 
-1. Run with:
+1. Might wanna install:
+
+	- Node.js
+	- MongoDb
+	- Conemu console
+
+2. Might wanna run:
+
+	- npm install
+
+3. Run options:
 
 	- run app
 	- run test -unit
--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 
-if %1 == app (
+if "%1" == "app" (
 
 	cmd /k "mongod" -new_console:t:mongod
 	timeout 5
@@ -10,14 +10,14 @@ if %1 == app (
 	echo. & echo App started
 )
 
-if %1 == test (
+if "%1" == "test" (
 
-	if %2 == -unit (
+	if "%2" == "-unit" (
 		cmd /k "karma start karma.conf.js" -new_console:t:unit
 		echo. & echo Unit tests started
 	)
 
-	if %2 == -e2e (
+	if "%2" == "-e2e" (
 		cmd /k "mongod" -new_console:t:mongod
 		cmd /k "ls" -new_console:d:"%cd%\tests\end-to-end"
 		echo. & echo End to end tests started
